1fbabecb145e9f1bcacb3e9be26aea1cb9f10517
Marc Bosch Matas <mboschmatas@gmail.com>
2023-04-17 09:26:28 +0200

    amended gitignore and documentation

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Correspondence of amenity categories as defined by the Catalan government and OSM tags
+#'
+#' A dataset containing every type of amenity as detected by he Catalan government linking it to one or several key/Value OSM pairs
+#'
+#' \itemize{
+#'   \item categoria_1. Main category of amenity
+#'   \item categoria_2. Secondary category of amenity.
+#'   \item categoria_3. Optional tertiary category of amenity
+#'   \item key. Corresponding OSM key
+#'   \item value. Corresponding OSM value.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name correspondences
+#' @usage data(correspondences)
+#' @format A tibble with 340 rows and 5 variables
+NULL
