e2ffecd52e230a98371c03e2d63e79254e7d38c1
marcboschmatas <mboschmatas@gmail.com>
2023-05-12 19:02:55 +0200

    added test

---
+++ tests/testthat/test_school_amenity.R
@@ -0,0 +1,13 @@
+require(testthat)
+devtools::load_all()
+context("Check that all detected amenities correspond to the expected types")
+df <- get_all_schools("Palafrugell", plot = FALSE)
+
+nrow_df_filtered <- df |> 
+  filter(!(osm_amenity %in% c("school", "kindergarten", "music_school", "dancing_school", "language_school", NA_character_))) |> 
+  nrow()
+
+test_that("Only imports amenities which correspond to the expected types", {
+  expected.value <- 0L
+  expect_identical(expected.value, nrow_df_filtered)
+})
