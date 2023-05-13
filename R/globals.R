04cfb414162493bfc7c97d57ff47ba5c0d13aed7
marcboschmatas <mboschmatas@gmail.com>
2023-05-13 16:50:34 +0200

    finished cleaning tests

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
-utils::globalVariables(c("Cycle", "ISCED", "level", "adre_a", "amenity", "codi_comarca",
+utils::globalVariables(c("Cycle", "ISCED level", "adre_a", "amenity", "codi_comarca",
                          "codi_delegaci", "codi_districte_municipal", "codi_localitat", "codi_municipi", "codi_municipi_6",
                          "codi_naturalesa", "codi_titularitat", "contact:fax", "contact:phone", 
                          "coordenades_geo_x", "coordenades_geo_y", "curs", "geometry", "junk", "max_age", 
